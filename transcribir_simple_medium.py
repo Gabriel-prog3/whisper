b90fe62b00fce0ac16da60a3609a8068198834a0
Gabriel Revelo <gabriel.revelo@stockviz.com>
2024-03-14 14:34:35 -0500

    word

--- transcribir_simple_medium.py
+++ transcribir_simple_medium.py
@@ -1,16 +1,32 @@
 import os
 import whisper
+from docx import Document
 
 # Get filenames in the current directory
 filenames = os.listdir()
 
 model = whisper.load_model("medium")
 
+text = ''
+
 # Print each filename
 for filename in filenames:
     try:
         result = model.transcribe(filename)
-        print(filename)
-        print(result["text"],'\n\n')
+        text += filename + '\n\n' + result["text"] + '\n\n\n'
     except:
         pass 
+
+
+# Create a new document object
+document = Document()
+
+# Add a paragraph to the document
+paragraph = document.add_paragraph()
+
+# Add the string to the paragraph
+paragraph.add_run(text)
+
+# Save the document with a filename
+document.save("my_document.docx")
+
